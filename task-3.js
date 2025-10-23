917d03254a73bdd17e1945f977276299f3302b2e
Batmar78 <igorovyc@gmail.com>
2025-10-24 00:20:05 +0300

    wtite task 3.js

--- task-3.js
+++ task-3.js
@@ -5,3 +5,29 @@ function circleCircumference(circle) {
   return +(2 * Math.PI * circle.radius).toFixed(6);
 }
 
+// Training JS #12: loop statement --for..in and for..of
+// https://www.codewars.com/kata/training-js-number-12-loop-statement-for-dot-in-and-for-dot-of/train/javascript
+
+function giveMeFive(obj){
+  const result = [];
+  for (let key in obj) {
+    if (key.length === 5) result.push(key);
+    if (obj[key].length === 5) result.push(obj[key]);
+  }
+  return result;  
+}
+
+// Understanding closures - the basics
+// https://www.codewars.com/kata/understanding-closures-the-basics/train/javascript
+
+function buildFun(n){
+
+	const arrFunc = []
+
+	for (let i = 0; i< n; i++){
+		arrFunc.push(function(){
+			return i;
+		})
+	}
+	return arrFunc
+}
